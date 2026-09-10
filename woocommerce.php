ce26d0193a381eb7c554722282e252b283b4450d
ishimwe isaac <122705379+isshiimwe@users.noreply.github.com>
2026-09-10 12:56:29 -0800

    v1.4.0 - Custom product cards and shop grid

--- woocommerce.php
+++ woocommerce.php
@@ -1,6 +1,12 @@
-<?php get_header(); ?>
+<?php
+/**
+ * WooCommerce template wrapper.
+ * For single products, cart, checkout, account pages.
+ * The shop/archive uses woocommerce/archive-product.php
+ */
+get_header(); ?>
 
-<main style="padding:7rem 0 4rem;">
+<main style="padding-top:56px;min-height:60vh;">
   <?php woocommerce_content(); ?>
 </main>
 
